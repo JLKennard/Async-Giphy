8f3155879c871bee240fc717f5806e5da088b5e6
JLKennard <jessicakennard@hotmail.co.uk>
2023-05-02 23:25:40 +0100

    updated logic

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-require('dotenv').config();
+// require("dotenv").config();
+//console.log(process.env.API_KEY);
+// const API_KEY =
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`;
+
+const imgCont = document.querySelector("#image-container");
+const textInput = document.querySelector("#text-input");
+const button = document.querySelector("#button");
+
+// fetch insert gif to html
+
+// async function getImage(query) {
+//   const result = await fetch(endpoint);
+//   const content = await result.json();
+//   const gif = content.data[0].images.downsized.url;
+//   const title = content.data[0].title;
+//   imgCont.innerHTML = `<img src="${gif}" alt="${title}"></img>`;
+// }
+// getImage();
+
+button.addEventListener("click", async () => {
+  console.log(textInput.value);
+  const result = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${textInput.value}&limit=25&offset=0&rating=g&lang=en`
+  );
+  const content = await result.json();
+  const randomGif =
+    content.data[Math.floor(Math.random() * content.data.length)];
+  const gif = randomGif.images.downsized.url;
+  const title = randomGif.title;
+  imgCont.innerHTML = `<img src="${gif}" alt="${title}"></img>`;
+});
